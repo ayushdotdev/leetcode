a9903d7ffa910c30aca509a33766bcce707d0a7c
ayushdotdev <97295627+ayushdotdev@users.noreply.github.com>
2022-05-08 06:15:33 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (33.33%) - LeetHub

---
+++ 492-construct-the-rectangle/492-construct-the-rectangle.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    
+public:
+    vector<int> constructRectangle(int area) {
+        vector<int> ans;
+        int i;
+        for(i=floor(sqrt(area)); i>=1; i--){
+            if( (area%i) == 0){
+                 ans.push_back(area/i);
+                ans.push_back(i);
+                break;
+            }
+        }
+       
+        return ans;
+    }
+};
